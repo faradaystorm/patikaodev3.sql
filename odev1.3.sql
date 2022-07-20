33395631bb8c1f6bbf8241d33f0cceb370e23a88
faradaystorm <96180440+faradaystorm@users.noreply.github.com>
2022-07-20 17:53:13 +0300

    Create odev1.3.sql

---
+++ odev1.3.sql
@@ -0,0 +1,3 @@
+--film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+select title from film;
+where title ilike '%T%T%T%T%'
