66a82d6982b10b1a5dd4c2b2e9450e7e008ecc37
faradaystorm <96180440+faradaystorm@users.noreply.github.com>
2022-07-20 17:55:14 +0300

    Create odev1.4.sql

---
+++ odev1.4.sql
@@ -0,0 +1,3 @@
+--film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+select * from film;
+where like '%C' and length>90 and rental_rate=2.99
