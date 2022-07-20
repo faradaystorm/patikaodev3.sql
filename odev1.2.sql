a822651f0de2b64c28e147f821b04e760e51c9ef
faradaystorm <96180440+faradaystorm@users.noreply.github.com>
2022-07-20 17:49:45 +0300

    Create odev1.2.sql

---
+++ odev1.2.sql
@@ -0,0 +1,3 @@
+--country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+select * from country;
+where country like '_____%n'
